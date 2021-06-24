77dd58e8cad9c6fce5baf1bc3aa535665bfd89de
yaofeng-wang <yaofeng96@gmail.com>
2021-06-24 16:24:34 +0800

    refactor settings file

--- backend/backend/settings_prod.py
+++ backend/backend/settings_prod.py
@@ -4,7 +4,6 @@ from .settings_common import *
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
 
-
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
@@ -17,24 +16,18 @@ DATABASES = {
         'NAME': os.environ['POSTGRES_DB'],
         'USER': os.environ['POSTGRES_USER'],
         'PASSWORD': os.environ['POSTGRES_PASSWORD'],
-        'HOST': 'db',
-        'PORT': 5432,
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': os.environ['POSTGRES_PORT'],
     }
 }
 
 
 # Static files (CSS, JavaScript, Images)
-
 STATIC_URL = '/static/'
-
 REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
-
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static'),
 ]
 
 # Default primary key field type
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ORIGIN_ALLOW_ALL = True
