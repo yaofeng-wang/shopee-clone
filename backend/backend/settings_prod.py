6b8f4230384eaef33c70d8d6a6687d6f3953ccba
yaofeng-wang <yaofeng96@gmail.com>
2021-06-24 17:28:56 +0800

    config s3 bucket

--- backend/backend/settings_prod.py
+++ backend/backend/settings_prod.py
@@ -23,11 +23,18 @@ DATABASES = {
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
 REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static'),
 ]
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -2,5 +2,14 @@
 
 python manage.py makemigrations --no-input;
 python manage.py migrate --no-input;
+python manage.py collectstatic --no-input;
+
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL;
+fi
 
 gunicorn backend.wsgi:application -c gunicorn.conf.py
