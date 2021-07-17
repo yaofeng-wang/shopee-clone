d4e3e57e2ac5cd2d69039fe4362ebe62dad23d71
yaofeng-wang <yaofeng96@gmail.com>
2021-07-17 13:54:53 +0800

    update models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+
 class Product(models.Model):
+    seller = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    # price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     # image_url = models
 
     
