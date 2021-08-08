9555db569cf402209c0a1d28e35f8660f2582f92
yaofeng-wang <yaofeng96@gmail.com>
2021-08-08 20:17:19 +0800

    update product details

--- backend/frontend/src/components/ProductDetail.js
+++ backend/frontend/src/components/ProductDetail.js
@@ -1,7 +1,10 @@
-import { Container, Row, Col } from "react-bootstrap";
 import useFetch from "./useFetch";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 export default function ProductDetail() {
   const { id } = useParams();
@@ -13,12 +16,27 @@ export default function ProductDetail() {
   );
 
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h1 className="text-center">{!isLoading && product.name}</h1>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      {isLoading ? (
+        <div>Loading</div>
+      ) : (
+        <Container>
+          <Row>
+            <Col>
+              <img style={{ width: "100%" }} src={product.image}></img>
+            </Col>
+            <Col>
+              <div>
+                <h3 className="text-center">Name: {product.name}</h3>
+                <h3 className="text-center">Price: S${product.price}</h3>
+              </div>
+              <Button variant="light" style={{ width: "100%" }}>
+                Add to Cart
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      )}
+    </>
   );
 }
